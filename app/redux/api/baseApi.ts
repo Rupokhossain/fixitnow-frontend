540e9245ddba24ac38a64125a49b276615151ded
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-08-02 15:49:35 +0600

    update env url

--- app/redux/api/baseApi.ts
+++ app/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL,
     credentials: "include", 
   }),
   tagTypes: ["Services", "Bookings", "Users","Categories"],
